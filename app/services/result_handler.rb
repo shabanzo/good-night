bdf6240cc632cc321dcf4d581f3a91a61c7168a7
Muhamad Rizky Syaban <rizkyshaban@gmail.com>
2023-03-23 11:28:25 +0700

    Refactor - Move messages to ResultHandler

--- app/services/result_handler.rb
+++ app/services/result_handler.rb
@@ -16,4 +16,42 @@ module ResultHandler
       failure:  payload
     )
   end
+
+  # Error messages
+  def em_user_not_found
+    "Not found; User not found!"
+  end
+
+  def em_target_user_not_found
+    "Not found; Target user not found!"
+  end
+
+  def em_incomplete_history
+    "Bad request; You have incomplete sleep history! Please clock it out first!"
+  end
+
+  def em_already_following
+    "Bad request; You are already following the target user!"
+  end
+
+  def em_already_unfollowed
+    "Bad request; You've already unfollowed the target user!"
+  end
+
+  def em_failed_to_save(obj)
+    "Failed to save; #{obj.errors.full_messages.join('; ')}"
+  end
+
+  # Success messages
+  def sm_clock_in
+    'Congratulations, your clock-in has been recorded successfully!'
+  end
+
+  def sm_follow
+    'You are now following this user!'
+  end
+
+  def sm_unfollow
+    'You are now unfollow this user!'
+  end
 end
