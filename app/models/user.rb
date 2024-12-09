fe6f70673bd6c041ed83e8c4fa7aa66311cb5b97
shabanzokp <smuhamad@kinetixpro.ai>
2024-12-09 12:30:08 +0700

    model: create clock_in and clock_out func

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Follow', foreign_key: 'followee_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followee
   has_many :followers, through: :passive_relationships, source: :follower
+
+  def clock_in!
+    sleep_histories.create
+  end
+
+  def clock_out!
+    sleep_histories.incomplete.last.update clock_out_time: DateTime.current
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:target_user) { create(:user) }
+
+  describe '#clock_in!' do
+    it 'creates a new sleep_history' do
+      expect {
+        user.clock_in!
+      }.to change(user.sleep_histories, :count).by(1)
+    end
+  end
+
+  describe '#clock_out!' do
+    let(:incomplete_sleep_history) { create(:incomplete_sleep_history, user: user) }
+
+    before do
+      incomplete_sleep_history
+    end
+
+    it 'updates an incomplete sleep_history to be completed' do
+      expect {
+        user.clock_out!
+      }.to change(user.sleep_histories.incomplete, :count).by(-1)
+    end
+  end
 end
